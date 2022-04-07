9909546932bf35ccb92cd5af7ddb16642979d870
Madhu B <b.madhu@ab-inbev.com>
2022-04-07 18:07:19 +0530

    Added additional queries

--- 2-SQL Server Assignment.sql
+++ 2-SQL Server Assignment.sql
@@ -4,7 +4,7 @@
 Select * from Customer where country = 'Germany';
 
 --Display the  full name of the employee
---Creating employee TABLE
+	--Creating employee TABLE
 
 CREATE TABLE Employee
 (
@@ -23,32 +23,40 @@ VALUES ('Manjunath','M','Bangalore','India','82827393933'),
 Select CONCAT(FirstName, ' ', LastName) as 'Name' from Customer;
 
 --Display the  customer details  who has Fax number
---Add FaxNumber column
+	--Add FaxNumber column
 ALTER TABLE Customer
 ADD FaxNumber INT;
 
 Select * from Customer where faxnumber is not null;
+
 --display the customer details whose name holds second letter as U
-Select * from customer where SUBSTRING(Concate(Firstname,LastName),2,1) = 'U'
+Select * from customer where SUBSTRING(Concate(Firstname,LastName),2,1) = 'U';
+
 --select order Details where unit price is greater than 10 and less than 20
 Select * from Order where unitprice > 10 and unitprice < 20;
+
 --Display order details which contains shipping date and arrange the order by date
-Select OrderDate as [Shippping_Date]* from Order order by OrderDate
+Select OrderDate as [Shippping_Date]* from Order order by OrderDate;
+
 --Print the orders shipped by ship name 'La corned'abondance' between 2 dates(Choose dates of your choice)
 --Add column ShippedName
 ALTER TABLE ORDER
-ADD ShippedName nvarchar(40)
+ADD ShippedName nvarchar(40);
 
 Select * from Order where ShippedName ='La corned''abondance' and OrderDate Between '2022-03-15' and '2022-04-04';
 
 --Print the products supplied by 'Exotic Liquids'
-
 Select * from Product where package = 'Exotic Liquids'
 
 --print the average quantity ordered for every product
-
+Select AVG(Quantity),ProductId from OrderItem  group by ProductID
 --Print all the Shipping company name and the ship names if they are operational
+	--Adding Shipping operationl COLUMNS
+ALTER TABLE ORDER
+ADD isShippingOperation bit(40);
 
+Select ShippedName from Order where isShippingOperation = 1;
+	
 --Print all Employees with Manager Name
 
 --Print the bill for a given order id .bill should contain Productname, Categoryname,price after discount
